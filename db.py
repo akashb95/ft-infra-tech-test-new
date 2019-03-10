43835c8c5880a75425a7988cac141117faab3df6
Akash B <akashb50@yahoo.co.uk>
2019-03-10 17:17:35 +0000

    db connection
    Signed-off-by: Akash B <akashb50@yahoo.co.uk>

---
+++ db.py
@@ -0,0 +1,12 @@
+import boto3
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def dyn_connection():
+    dyn = boto3.resource("dynamodb",
+                         aws_access_key_id=os.getenv("AWS_ACCESS"), aws_secret_access_key=os.getenv("AWS_SECRET"),
+                         region_name=os.getenv("AWS_REGION"), endpoint_url=os.getenv("DYNAMO_ENDPOINT"))
+    table = dyn.Table(os.getenv("DYNAMO_TABLE"))
+    return table
